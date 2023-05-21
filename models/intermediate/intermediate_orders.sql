1a5f3d61d4817cf66dde59083d7efce7ba4134ec
100346844@alumnos.uc3m.es <100346844@alumnos.uc3m.es>
2023-05-21 19:29:35 +0000

    intermedio creado

--- models/intermediate/intermediate_orders.sql
+++ models/intermediate/intermediate_orders.sql
@@ -5,32 +5,48 @@ with orders as (
 ),
 
 
-customer as (
+customers as (
 
     select * from {{ ref('customer') }}
 
 ),
 
+nation as (
+    select * from {{ ref('nation') }}
+),
+
+region as (
+    select * from {{ ref('region') }}
+),
+
+
 --Cogemos el valor total de todos los pedidos
 
-order_totals as (
+order_total as (
 
-    select orderkey
-    sum (totalprice) as order_value
+    select 
+        orderkey,
+        sum (totalprice) as order_value
     from orders
-    group by 1,2
+    group by 1
 
 ),
 
 high_priority_orders as(
     select
         orders.orderkey,
-        orders.custkey
+        orders.custkey,
         orders.orderpriority,
-        order_totals.order_value
+        nation.name as nation,
+        region.name as region
+        --order_total.order_value
+
     from orders
+    --left join order_total on order_total.orderkey=orders.orderkey
     left join customers on orders.custkey=customers.custkey
-    where orders.ordepriority='1-URGENT'
+    left join nation on nation.nationkey=customers.nationkey
+    left join region on region.regionkey=nation.regionkey
+    where orders.orderpriority='1-URGENT'
 )
 
 select * from high_priority_orders
